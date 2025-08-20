68685de51af6307d03b0a8fe88aebdf92bfb4f35
Soihoanghuhong293 <pxq293it@gmail.com>
2025-08-20 16:50:35 +0700

    gh

--- commons/env.php
+++ commons/env.php
@@ -2,8 +2,8 @@
 
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
-define('BASE_URL', 'http://localhost/DA.1_N5/');
-define('BASE_URL_ADMIN', 'http://localhost/DA.1_N5/admin/');
+define('BASE_URL', 'http://localhost/duAn1/');
+define('BASE_URL_ADMIN', 'http://localhost/duAn1/admin/');
 
 
 //define('BASE_URL'       , 'http://localhost:8001/');
@@ -15,6 +15,6 @@ define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'phoneweb11');  // Tên database
+define('DB_NAME'    , 'phoneweb');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
