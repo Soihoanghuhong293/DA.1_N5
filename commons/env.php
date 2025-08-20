bd531834d381c140d189dd952fb2acefba797d87
Soihoanghuhong293 <pxq293it@gmail.com>
2025-08-20 13:54:17 +0700

    sd

--- commons/env.php
+++ commons/env.php
@@ -15,6 +15,6 @@ define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'phoneweb');  // Tên database
+define('DB_NAME'    , 'phoneweb11');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
